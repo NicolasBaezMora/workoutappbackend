27bc31f8b42393348534278bf892fde5def1db46
EzabAmro <baezmoranicolas@gmail.com>
2021-07-14 21:00:23 -0500

    Project backend started

---
+++ src/main/java/com/workoutapp/workoutappBackend/WorkOutAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workoutapp.workoutappBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOutAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOutAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workoutapp/workoutappBackend/WorkOutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workoutapp.workoutappBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
